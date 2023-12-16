5402f52445acd90c043947315b4b5a85aa85c703
daniel-antonio-developer <danielantonio.dad52@gmail.com>
2023-12-16 14:02:22 +0100

    Atualização da estilização dos botões e lógica sobre os eventos

--- statevent/src/App.js
+++ statevent/src/App.js
@@ -1,16 +1,29 @@
 
 import './App.css';
 
+
+const messages = [
+  "Fundamentos de React.js",
+    "React.js Avançado",
+    "React.js + Node.js",
+
+]
+
 function App() {
+
+  const step = 1;
+  const activeColor = 'green'; // Cor para botões ativos
+  const inactiveColor = 'gray'; // Cor para botões inativos
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`number ${step >= 1 ? "active" : ""}`} style={{ backgroundColor: step >= 1 ? activeColor : inactiveColor }}>1</div>
+        <div className={`number ${step >= 2 ? "active" : ""}`} style={{ backgroundColor: step >= 2 ? activeColor : inactiveColor }}>2</div>
+        <div className={`number ${step >= 3 ? "active" : ""}`} style={{ backgroundColor: step >= 3 ? activeColor : inactiveColor }}>3</div>
       </div>
 
-      <p className="message"> Hello</p>
+      <p className="message"> Step{step}: {messages[step-1]}</p>
       <div className="buttons">
          <button className="">Prev</button>
         <button className="">Next</button>
