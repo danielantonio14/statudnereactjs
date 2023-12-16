4261740aa85c014a3c0915c467da5d965ebbb3b8
daniel-antonio-developer <danielantonio.dad52@gmail.com>
2023-12-16 10:30:57 +0100

    Adicionei uma div para servir de container para o content

--- statevent/src/App.js
+++ statevent/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div classe = 'jfdkf'> 
-
-                
-
-    </div>
+    <div className="steps"></div>
 
   );
 }
